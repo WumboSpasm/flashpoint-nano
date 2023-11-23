ebe995369e63d73a4f25c164e4dbfa0d6929b8b7
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-11-22 20:41:04 -0500

    Add support for Legacy entries

--- flashpoint.sh
+++ flashpoint.sh
@@ -39,11 +39,14 @@ if [[ ! -f $database_file ]]; then
 fi
 
 echo $0: querying database for entry properties corresponding to ID $entry_id
-entry_query=$(sqlite3 --separator $'\n' "file:${database_file}?mode=ro" ".param set :id $entry_id" "select path,platformName,game_data.applicationPath,game_data.launchCommand from game_data join game on game_data.gameId = game.id where gameId = :id" 2> /dev/null)
+entry_query=$(sqlite3 --separator $'\n' "file:${database_file}?mode=ro" ".param set :id $entry_id" "select game_data.path, game.platformName, coalesce(game_data.applicationPath, game.applicationPath), coalesce(game_data.launchCommand, game.launchCommand) from game left join game_data on game.id = game_data.gameId where game.id = :id" 2> /dev/null)
 [[ $? > 0 ]] && error_invalid_database
 
 readarray -t entry_properties <<< $entry_query
 [[ ${#entry_properties[@]} == 0 ]] && error_invalid_id
+
+entry_is_legacy=false
+[[ ${#entry_properties[0]} == 0 ]] && entry_is_legacy=true
 
 entry_gamedata_file=${entry_properties[0]}
 entry_primary_platform=${entry_properties[1]}
@@ -52,7 +55,7 @@ entry_launch_command=${entry_properties[3]}
 
 if [[ ${#addapp_id} > 0 ]]; then
 	echo $0: querying database for additional app properties corresponding to ID $addapp_id
-	addapp_query=$(sqlite3 --separator $'\n' "file:${database_file}?mode=ro" ".param set :id $addapp_id" "select applicationPath,launchCommand from additional_app where id = :id" 2> /dev/null)
+	addapp_query=$(sqlite3 --separator $'\n' "file:${database_file}?mode=ro" ".param set :id $addapp_id" "select applicationPath, launchCommand from additional_app where id = :id" 2> /dev/null)
 	
 	readarray -t addapp_properties <<< $addapp_query
 	if [[ ${#addapp_properties[@]} > 0 ]]; then
@@ -85,24 +88,26 @@ for i in $(seq 0 2 $((${#platform_overrides[@]}-1))); do
 done
 [[ $found_match == false ]] && error_unsupported_entry
 
-[[ ! -d $gamedata_path ]] && mkdir -p "$gamedata_path"
-if [[ ! -f "$gamedata_path/$entry_gamedata_file" ]]; then
-	for gamedata_source in ${gamedata_sources[@]}; do
-		echo $0: downloading $gamedata_source/$entry_gamedata_file
-		curl "$gamedata_source/$entry_gamedata_file" -sfo "$gamedata_path/$entry_gamedata_file"
+if [[ $entry_is_legacy == false ]]; then
+	[[ ! -d $gamedata_path ]] && mkdir -p "$gamedata_path"
+	if [[ ! -f "$gamedata_path/$entry_gamedata_file" ]]; then
+		for gamedata_source in ${gamedata_sources[@]}; do
+			echo $0: downloading $gamedata_source/$entry_gamedata_file
+			curl "$gamedata_source/$entry_gamedata_file" -sfo "$gamedata_path/$entry_gamedata_file"
 		
-		[[ $? == 0 ]] && break
-		echo $0: failed to download from desired source
-	done
+			[[ $? == 0 ]] && break
+			echo $0: failed to download from desired source
+		done
 
-	[[ ! -f "$gamedata_path/$entry_gamedata_file" ]] && error_all_downloads_failed "$entry_gamedata_file"
+		[[ ! -f "$gamedata_path/$entry_gamedata_file" ]] && error_all_downloads_failed "$entry_gamedata_file"
+	fi
 fi
 
 echo $0: initializing game server
 (
 	trap "kill 0" SIGINT
-	exec "./$gameserver_file" -rootPath= -gameRootPath="$gamedata_path" -legacyHTDOCSPath="$legacy_path" &
-	curl -X POST -d "{\"filePath\":\"$entry_gamedata_file\"}" http://localhost:22501/fpProxy/api/mountzip &
+	exec "./$gameserver_file" -rootPath= -gameRootPath="$gamedata_path" -legacyHTDOCSPath="$legacy_path" -handleLegacyRequests=true -useInfinityServer=true &
+	[[ $entry_is_legacy == false ]] && curl -X POST -d "{\"filePath\":\"$entry_gamedata_file\"}" http://localhost:22501/fpProxy/api/mountzip &
 	source "./$entry_application_path" &
 	wait
 )
