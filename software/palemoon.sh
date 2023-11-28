e9776d39f896a64ddec1845392700828654c5c9b
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-11-28 17:50:31 -0500

    Back up Pale Moon signatures correctly, improve mkdir behavior

--- software/palemoon.sh
+++ software/palemoon.sh
@@ -2,8 +2,8 @@ external_signature=$(curl -Lsf "https://www.palemoon.org/download.php?mirror=sig
 if [[ ! -f software/palemoon/palemoon.sig || $external_signature != $(cat software/palemoon/palemoon.sig) ]]; then
 	echo $0: downloading latest Pale Moon release
 	curl -Lsf "https://www.palemoon.org/download.php?mirror=us&bits=64&type=linuxgtk3" | unxz | tar xC software &> /dev/null
-	echo $external_signature > software/palemoon/palemoon.sig
-	mkdir software/palemoon/profile
+	echo "$external_signature" > software/palemoon/palemoon.sig
+	mkdir -p software/palemoon/profile
 fi
 
 echo $0: launching $entry_launch_command using Pale Moon
--- software/ruffle.sh
+++ software/ruffle.sh
@@ -2,7 +2,7 @@ if [[ ! -f software/ruffle/build || $(find software/ruffle/ruffle -mtime +0) ]];
 	external_build=$(curl -s https://api.github.com/repos/ruffle-rs/ruffle/releases?per_page=1 | grep -Eom1 '"Nightly .*?"')
 	if [[ $external_build != $(cat software/ruffle/build 2> /dev/null) ]]; then
 		echo $0: downloading latest Ruffle nightly
-		[[ ! -d software/ruffle ]] && mkdir -p software/ruffle
+		mkdir -p software/ruffle
 		curl https://github.com/$(curl -s https://api.github.com/repos/ruffle-rs/ruffle/releases?per_page=1 | grep -Eom1 ruffle-rs/.*?linux-x86_64.tar.gz) -Lsf | tar xzC software/ruffle &> /dev/null
 		echo $external_build > software/ruffle/build
 	fi
